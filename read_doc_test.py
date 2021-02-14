daea2a7f7394a4c0aeca999f396f561b8622c4c0
Trent <trentwisecup@gmail.com>
2021-02-14 12:13:23 -0600

    Reading tables

--- read_doc_test.py
+++ read_doc_test.py
@@ -6,3 +6,23 @@ all_paras = doc.paragraphs
 for para in all_paras:
     print(para.text)
     print("-------")
+
+data = []
+
+table = doc.tables[0]
+for i, row in enumerate(table.rows):
+    text = (cell.text for cell in row.cells)
+    #print(row.cells)
+    
+    # Establish the mapping based on the first row
+    # headers; these will become the keys of our dictionary
+    if i == 0:
+        keys = tuple(text)
+        continue
+
+    # Construct a dictionary for this row, mapping
+    # keys to values for this row
+    row_data = dict(zip(keys, text))
+    data.append(row_data)
+
+print(data)
