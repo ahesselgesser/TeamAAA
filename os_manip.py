545d2dfe0202b29e63300b2aeb79eda24cfe2bc6
trentwisecup <trentwisecup@gmail.com>
2021-02-21 13:35:23 -0600

    Copy file, and unzip it. DON'T RUN, it doesn't unzip into its own directory.

--- os_manip.py
+++ os_manip.py
@@ -1,12 +1,20 @@
 import os
 from shutil import copyfile
+import zipfile
 
 # 'C:\Users\twins\Desktop\UNO classes\Spring 2021 Semester\CSCI 4970 - Capstone\Python tests'
 # https://github.com/ahesselgesser/TeamAAA
 
-source = input("Enter the source location of the file: example C:/Users/bob/word.docx\n")
+source_dir = input("Enter the source location of the file: example C:/Users/bob/\n")
+source_fn = input("Enter the name of the file to copy\n")
 
-destination = input("Enter the destination and name of the file: example C:/Users/bob/word_2.docx\n")
+#destination_dir = input("Enter the destination and name of the file: example C:/Users/bob/word_2.docx\n")
+destination_fn = input("Enter the name of the copied file\n")
 
 # copyfile(source, destination)
-copyfile(source, destination)
+copyfile(source_dir + "\\" + source_fn, source_dir + "\\" + destination_fn)
+
+print(source_dir + destination_fn)
+
+with zipfile.ZipFile(source_dir + "/" + destination_fn, 'r') as zip_ref:
+    zip_ref.extractall(source_dir)
