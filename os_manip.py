784059b24a5507876c44f18da0f0b3273c0a3048
trentwisecup <trentwisecup@gmail.com>
2021-02-21 13:44:25 -0600

    Added user prompt to enter directory to unzip to

--- os_manip.py
+++ os_manip.py
@@ -14,7 +14,9 @@ destination_fn = input("Enter the name of the copied file\n")
 # copyfile(source, destination)
 copyfile(source_dir + "\\" + source_fn, source_dir + "\\" + destination_fn)
 
-print(source_dir + destination_fn)
+print(source_dir + "/" destination_fn)
+
+zip_dir = input("Enter name of directory to unzip file\n")
 
 with zipfile.ZipFile(source_dir + "/" + destination_fn, 'r') as zip_ref:
-    zip_ref.extractall(source_dir)
+    zip_ref.extractall(source_dir + "/" + zip_dir)
