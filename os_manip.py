1d24bb343dfcbf41f30b27a0321c18558c3e0351
trentwisecup <trentwisecup@gmail.com>
2021-02-21 12:53:54 -0600

    Copying file AND renaming as a ZIP, it works

--- os_manip.py
+++ os_manip.py
@@ -4,4 +4,4 @@ from shutil import copyfile
 # 'C:\Users\twins\Desktop\UNO classes\Spring 2021 Semester\CSCI 4970 - Capstone\Python tests'
 # https://github.com/ahesselgesser/TeamAAA
 
-copyfile('C:/Users/twins/Desktop/UNO classes/Spring 2021 Semester/CSCI 4970 - Capstone/Python tests/grad_chxBox_test.docx', 'C:/Users/twins/Desktop/UNO classes/Spring 2021 Semester/CSCI 4970 - Capstone/Python tests/grad_chxBox_test_2.docx')
+copyfile('C:/Users/twins/Desktop/UNO classes/Spring 2021 Semester/CSCI 4970 - Capstone/Python tests/grad_chxBox_test.docx', 'C:/Users/twins/Desktop/UNO classes/Spring 2021 Semester/CSCI 4970 - Capstone/Python tests/grad_chxBox_test_2.zip')
