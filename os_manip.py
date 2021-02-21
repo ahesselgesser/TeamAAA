5263a9f6d60b52d1381dd00a02548acc2aab639c
trentwisecup <trentwisecup@gmail.com>
2021-02-21 13:00:42 -0600

    Added prompt for source and destination of file

--- os_manip.py
+++ os_manip.py
@@ -4,4 +4,9 @@ from shutil import copyfile
 # 'C:\Users\twins\Desktop\UNO classes\Spring 2021 Semester\CSCI 4970 - Capstone\Python tests'
 # https://github.com/ahesselgesser/TeamAAA
 
-copyfile('C:/Users/twins/Desktop/UNO classes/Spring 2021 Semester/CSCI 4970 - Capstone/Python tests/grad_chxBox_test.docx', 'C:/Users/twins/Desktop/UNO classes/Spring 2021 Semester/CSCI 4970 - Capstone/Python tests/grad_chxBox_test_2.zip')
+source = input("Enter the source location of the file: example C:/Users/bob/word.docx\n")
+
+destination = input("Enter the destination and name of the file: example C:/Users/bob/word_2.docx\n")
+
+# copyfile(source, destination)
+copyfile(source, destination)
