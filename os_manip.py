c901f34d79fd3a842660311033e614b327ad8292
trentwisecup <trentwisecup@gmail.com>
2021-02-21 12:51:42 -0600

    A test to copy files

---
+++ os_manip.py
@@ -0,0 +1,7 @@
+import os
+from shutil import copyfile
+
+# 'C:\Users\twins\Desktop\UNO classes\Spring 2021 Semester\CSCI 4970 - Capstone\Python tests'
+# https://github.com/ahesselgesser/TeamAAA
+
+copyfile('C:/Users/twins/Desktop/UNO classes/Spring 2021 Semester/CSCI 4970 - Capstone/Python tests/grad_chxBox_test.docx', 'C:/Users/twins/Desktop/UNO classes/Spring 2021 Semester/CSCI 4970 - Capstone/Python tests/grad_chxBox_test_2.docx')
