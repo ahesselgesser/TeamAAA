be44121ecb62badbd0d601aa940a1df25bd0077f
ahesselgesser <ahesselgesser@unomaha.edu>
2021-02-26 19:45:44 -0600

    added first part of pring string function

--- Objects/assessmentReport.py
+++ Objects/assessmentReport.py
@@ -13,7 +13,17 @@ class AssesssmentReport:
         self.resultCommunication = ""
     
     def toString(self):
-        print("This has not yet been implimented\n")
+        print("Year: " + self.year + "\n")
+        print("College: " + self.college + "\n")
+        print("Department: " + self.department + "\n")
+        print("Program: " + self.program + "\n")
+        print("Degree Level: " + self.degreeLevel + "\n")
+        print("Academic Year: " + self.academicYear + "\n")
+        print("Date Range of Data: " + self.personPreparingReport + "\n")
+        print("Person Preparing Report: " + self.year + "\n")
+        #SLO Stuff
+        print("SLO Communication" + self.sloCoumminication + "\n")
+        print("Communication Result:" + self.resultCommunication + "\n")
     
     def importToDB(self):
         print("This has not yet been implimented\n")
