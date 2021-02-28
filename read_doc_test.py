3025bd6055e4c9d0c8331991ca1697180efc0d00
trentwisecup <trentwisecup@gmail.com>
2021-02-28 14:17:38 -0600

    Updating docx parser with regex for college

--- read_doc_test.py
+++ read_doc_test.py
@@ -1,13 +1,30 @@
 import docx
 from docx.oxml.ns import qn
-doc = docx.Document("C:/Users/twins/Desktop/UNO classes/Spring 2021 Semester/CSCI 4970 - Capstone/Python tests/test.docx")
+import re
+
+## Change this file location to the location of the file you are parsing
+doc = docx.Document("C:/Users/twins/Desktop/UNO classes/Spring 2021 Semester/CSCI 4970 - Capstone/Python tests/undergrad2018-regular.docx")
+
+# 'C:\Users\twins\Desktop\UNO classes\Spring 2021 Semester\CSCI 4970 - Capstone\Python tests'
+# https://github.com/ahesselgesser/TeamAAA
+
+#### Variables to capture
+## College, Department/School ?, Program, Degree Level, Academic Year of Report,
+## Date Range of Reported Data, Person Preparing the Report, SLOs (maybe in an array, or tuple, or something)
+## Bloom's Taxonomy (checkboxes)
+
+
 
 all_paras = doc.paragraphs
 for para in all_paras:
-    print(para.text)
-    print("-------")
+    college_regex = re.findall('College:\s*(.*)\s*Department/School:', para.text)
+    if (college_regex):
+        print(college_regex[0])
+
+    #print(para.text)
 
 
+"""
 ### https://stackoverflow.com/questions/27861732/parsing-of-table-from-docx-file/27862205 ###
 data = []
 
@@ -29,3 +46,4 @@ for i, row in enumerate(table.rows):
 
 print(data)
 ###    https://stackoverflow.com/questions/27861732/parsing-of-table-from-docx-file/27862205 ###
+"""
