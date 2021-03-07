cdf4a6ff8960d1fe8bd8052f0667bc7ec4a5f4e6
trentwisecup <trentwisecup@gmail.com>
2021-03-07 11:46:59 -0600

    Combined the os_manip.py and read_doc_chx.py files

--- read_doc_chx.py
+++ read_doc_chx.py
@@ -1,6 +1,27 @@
+import os
+from shutil import copyfile
 import zipfile
 from lxml import etree
 import re
+
+# 'C:\Users\twins\Desktop\UNO classes\Spring 2021 Semester\CSCI 4970 - Capstone\Python tests'
+# https://github.com/ahesselgesser/TeamAAA
+
+source_dir = input("Enter the source location of the file: example C:/Users/bob/\n")
+source_fn = input("Enter the name of the file to copy\n")
+
+#destination_dir = input("Enter the destination and name of the file: example C:/Users/bob/word_2.docx\n")
+destination_fn = input("Enter the name of the copied file\n")
+
+# copyfile(source, destination)
+copyfile(source_dir + "\\" + source_fn, source_dir + "\\" + destination_fn)
+
+print(source_dir + "/" + destination_fn)
+
+zip_dir = input("Enter name of directory to unzip file\n")
+
+with zipfile.ZipFile(source_dir + "/" + destination_fn, 'r') as zip_ref:
+    zip_ref.extractall(source_dir + "/" + zip_dir)
 
 ### This path is relative, and it needs to be an XML document.
 xml_tree = etree.parse('C:/Users/twins/Desktop/UNO classes/Spring 2021 Semester/CSCI 4970 - Capstone/Python tests/grad_chxBox_test/word/document.xml')
@@ -19,7 +40,7 @@ prev_match = ""
 
 print("Here are the matches for checked box results!")
 for match in reg:
-    if (prev_match == "<w:t>&#9746;</w:t>"):
+    if (prev_match == "<w:t>&#9746;</w:t>" and match != '<w:t xml:space="preserve"> </w:t>'):
         print(prev_match + " " + match)
     prev_match = match
 
