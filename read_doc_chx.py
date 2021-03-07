8ead7bf6c0fc2dec250e6e4d9123ef9a62acf151
trentwisecup <trentwisecup@gmail.com>
2021-03-07 15:08:33 -0600

    Updated remove tags function and added limited ability to count # of SLOs

--- read_doc_chx.py
+++ read_doc_chx.py
@@ -10,24 +10,24 @@ slo_count = 0
 ## https://medium.com/@jorlugaqui/how-to-strip-html-tags-from-a-string-in-python-7cb81a2bbf44
 def remove_tags(text):
     """Remove < > tags from a string"""
-    clean = re.compile('<.*?>')
+    clean = '<.*?>\s*'
     return re.sub(clean, '', text)
 
 # 'C:\Users\twins\Desktop\UNO classes\Spring 2021 Semester\CSCI 4970 - Capstone\Python tests'
 # https://github.com/ahesselgesser/TeamAAA
 
-source_dir = input("Enter the source location of the file: example C:/Users/bob/\n")
-source_fn = input("Enter the name of the file to copy\n")
+source_dir = "C:\\Users\\twins\\Desktop\\UNO classes\\Spring 2021 Semester\\CSCI 4970 - Capstone\\Python tests"  # input("Enter the source location of the file: example C:/Users/bob/\n")
+source_fn =  "undergrad2018-regularv2.docx" #input("Enter the name of the file to copy\n")
 
 #destination_dir = input("Enter the destination and name of the file: example C:/Users/bob/word_2.docx\n")
-destination_fn = input("Enter the name of the copied file\n")
+destination_fn =  "undergrad2018-regularv2.zip" # input("Enter the name of the copied file\n")
 
 # copyfile(source, destination)
 copyfile(source_dir + "\\" + source_fn, source_dir + "\\" + destination_fn)
 
 print(source_dir + "/" + destination_fn)
 
-zip_dir = input("Enter name of directory to unzip file\n")
+zip_dir = "grad_chxBox_test" #input("Enter name of directory to unzip file\n")
 
 with zipfile.ZipFile(source_dir + "/" + destination_fn, 'r') as zip_ref:
     zip_ref.extractall(source_dir + "/" + zip_dir)
@@ -50,11 +50,12 @@ prev_match = ""
 print("Here are the matches for checked box results!")
 for index, match in enumerate(reg):
     if (prev_match == "<w:t>&#9746;</w:t>" ):
-        if (match == '<w:t xml:space="preserve"> </w:t>'):
-            match = reg[index+1]
-        print(remove_tags(match))
+        match = remove_tags(match)
+        print(match)
+        if (match in blooms_taxonomy_list):
+            slo_count += 1
     prev_match = match
-
+print("SLO Count = " + str(slo_count))
 # 'C:\Users\twins\Desktop\UNO classes\Spring 2021 Semester\CSCI 4970 - Capstone\Python tests'
 # https://github.com/ahesselgesser/TeamAAA
 # The CheckBox is &#9744;
