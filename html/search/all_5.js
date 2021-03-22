2d4d187483e6cbef7bcac7f83acaa84105c5ee8b
ahesselgesser <ahesselgesser@unomaha.edu>
2021-03-22 18:27:06 -0500

    Added auto generated doxygen html

---
+++ html/search/all_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['installed_5fapps_9',['INSTALLED_APPS',['../namespacemysite_1_1settings.html#ae0300504ffe212a8082806286d6fe36b',1,'mysite::settings']]]
+];
---
+++ html/search/all_8.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['root_5furlconf_18',['ROOT_URLCONF',['../namespacemysite_1_1settings.html#ab0a7fa8cdaaccbf5bf874db67cbe507a',1,'mysite::settings']]]
+];
