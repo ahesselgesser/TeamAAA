b89af254675e07a3a426df203dc287791e7a1f5f
ahesselgesser <ahesselgesser@unomaha.edu>
2021-04-11 13:09:06 -0500

    Added url for report page in urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('reports/upload/', views.upload_report, name='upload_report'),
     path('reports/<int:pk>/', views.delete_report, name='delete_report'),
     path('reports/search/', views.search, name='search'),
+    path('reports/view', views.view_report, name='report'),
 
     path('class/reports/', views.ReportListView.as_view(), name='class_report_list'),
     path('class/reports/upload/', views.UploadReportView.as_view(), name='class_upload_report'),
