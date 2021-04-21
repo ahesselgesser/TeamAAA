132f6d0699445b2ec44be6728d814d11a2e4b797
zijunmei <zijun.mei1993@gmail.com>
2021-04-21 08:01:11 -0500

    Update insertData.py

--- mysite/core/paser/insertData.py
+++ mysite/core/paser/insertData.py
@@ -4,7 +4,9 @@ def insertCheckBox(list_of_lists):
     conn = psycopg2.connect(database="aaadb", user='teamaaa', password='aaapass', host='localhost', port= '5432')
     conn.autocommit = True
     cursor = conn.cursor()
+
     cursor.execute()
+    
     conn.commit()
     print("Records inserted........")
 
