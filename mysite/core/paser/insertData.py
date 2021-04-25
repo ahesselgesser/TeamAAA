1bda6fe44734590abbf14e5aac9736e1a510a7d5
ahesselgesser <ahesselgesser@unomaha.edu>
2021-04-25 10:43:16 -0500

    Added placeholder inserting all other data in undergrad reports to insertData

--- mysite/core/paser/insertData.py
+++ mysite/core/paser/insertData.py
@@ -4,6 +4,7 @@ from mysite.core.models import basic_models
 from mysite.core.models import data_models
 from mysite.core.models import decisionsActions_models
 from mysite.core.models import slo_models
+from datetime import date
 import psycopg2
 def insertCheckBox(list_of_lists):
     conn = psycopg2.connect(database="aaadb", user='teamaaa', password='aaapass', host='localhost', port= '5432')
@@ -28,30 +29,63 @@ def insertReportHeader(match_list, list_of_lists, accredited1, length_slo, dec_a
     depart = aac_models.Department.objects.create(name=match_list[1],college=college1 ,active=True)
     depart.save()
     #insert Report table
-    report1 = basic_models.Report.objects.create(year=match_list[4], author= match_list[6], 
+    report = basic_models.Report.objects.create(year=match_list[4], author= match_list[6], 
         degreeProgram=match_list[2], accredited=accredited1, date_range_of_reported_data=match_list[5],
         section1Comment=None, section2Comment=None,section3Comment=None,section4Comment=None,submitted=True,
         returned=False,numberOfSLOs= length_slo, uploader= match_list[1])
     report1.save()
     #insert SLO table
-    slo1 = slo_models.SLO.objects.create(blooms=list_of_lists[0][1])
-    slo2 = slo_models.SLO.objects.create(blooms=list_of_lists[0][2])
-    slo3 = slo_models.SLO.objects.create(blooms=list_of_lists[0][3])
+    slos = []
+    slonum = 1
+    for blooms in list_of_lists[0]:
+        tempslo = slo_models.SLO.objects.create(blooms=list_of_lists[0][slonum], numberOfUses=1)
+        slos.append(tempslo)
+        #tempslo.save()
+        slonum = slonum + 1
     #insert SLO in Report Table
-    sloIR1 = slo_models.SLOInReport.objects.create(slo = slo1, report = report1, number = 1)
-    sloIR2 = slo_models.SLOInReport.objects.create(slo = slo2, report = report1, number = 2)
-    sloIR3 = slo_models.SLOInReport.objects.create(slo = slo3, report = report1, number = 3)
+    sloIRs = []
+    sloirnum = 1
+    for slo in slos: #TODO: Replace goal placeholder with goal text. Find how to find number of assess
+        sloIR = slo_models.SLOInReport.objects.create(goalText="Placeholder Text", slo=slo, changedFromPrior=False, report=report, number=sloirnum, numberOfAssess=0) #Number of asses unknown
+        sloirnum = sloirnum + 1
+        sloIRs.append(sloIR)
+        #sloIR.save()
+    #Assessment Methods
+    #TODO: All fields are currently placeholder because I do not know where the data is and due to a bug I can not run the parsing script
+    assessmentVs = []
+    #TODO: Add loop structure
+    assessment = assessment_models.Assessment.objects.create(title="Placeholder Title", domainExamination=False, domainProduct=False, domainPerformance=False, directMeasure=False, numberOfUses=1)
+    date = today.strftime("%d-%m-%Y")
+    assessmentVersion = assessment_models.AssessmentVersion.objects.create(report=report, slo=slos[0], number=0, changedFromPrior=False, assessment=assessment, date=date, description="Placeholder Description", finalTerm=False, where="Placeholder Location", allStudents=False, sampleDescription="Placholder sample description", frequencyChoice="O", frequency="Placeholder Frequency", threshold="Placeholder Threshold", target=0)
+    assessmentVs.append(assessmentVersion)
+    #Data Collection Methods
+    #TODO: All fields are currently placeholder because I do not know where the data is and due to a bug I can not run the parsing script
+    dataAdditional = data_models.DataAdditionalInformation.objects.create(report=report, comment="Placeholder Comment")
+    dataAdditional = data_models.ResultCommunicate.objects.create(text="Placeholder Communication", report=report)
+    for assessmentV in assessmentVs:
+        assessmentData = data_models.AssessmentData.objects.create(assessmentVersion = assessmentV,dataRange="Placeholder Data Range", numberStudents=1, overallProficient=1)
+        assessmentAgg = data_models.AssessmentAggregate.objects.create(assessmentVersion=assessmentV, aggregate_proficiency=1, met=False)
+        #assessmentData.save()
+        #assessmentAgg.save()
+    for sloIR in sloIRs:
+        sloStatus = data_models.SLOStatus(status="O", sloIR=sloIR, override=False)
+        #sloStatus.save()
+
+
     #insert decision Actions table
     for info in dec_act:
         if info[0] == "SLO 1":
             print(info[1])
-            decisionAction1 = decisionsActions_models.DecisionsActions.objects.create(sloIR = sloIR1, text=info[1])
+            decisionAction1 = decisionsActions_models.DecisionsActions.objects.create(sloIR = slos[0], text=info[1])
         elif info[0] == "SLO 2":
             print(info[1])
-            decisionAction1 = decisionsActions_models.DecisionsActions.objects.create(sloIR = sloIR2, text=info[1])
+            decisionAction1 = decisionsActions_models.DecisionsActions.objects.create(sloIR = slos[1], text=info[1])
         elif info[0] == "SLO 3":
             print(info[1])
-            decisionAction1 = decisionsActions_models.DecisionsActions.objects.create(sloIR = sloIR3, text=info[1])
+            decisionAction1 = decisionsActions_models.DecisionsActions.objects.create(sloIR = slos[2], text=info[1])
+        elif info[0] == "SLO 4":
+            print(info[1])
+            decisionAction1 = decisionsActions_models.DecisionsActions.objects.create(sloIR = slos[3], text=info[1])
     #
     print("Records inserted........")
 
