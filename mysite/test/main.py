884965a94326cd84481d5b5a70950b326662ab93
zijunmei <zijun.mei1993@gmail.com>
2021-05-02 00:34:20 -0500

    add id to delete report page button

--- mysite/test/main.py
+++ mysite/test/main.py
@@ -57,7 +57,7 @@ class Program_test_case(unittest.TestCase):
 
         id_test = str(self.driver.find_element_by_id("header_upload").text)
         self.assertEqual("Upload",id_test)
-    """
+    
     def test_file_upload_submit_with_files(self):
         element = WebDriverWait(self.driver, 10).until(
         EC.presence_of_element_located((By.ID, "Simple_Upload")))
@@ -75,6 +75,43 @@ class Program_test_case(unittest.TestCase):
 
         id_test = str(self.driver.find_element_by_id("upload_success").text)
         self.assertEqual("Successfully Uploaded!",id_test)
+    
+    def test_delete_reports_page(self):
+        element = WebDriverWait(self.driver, 10).until(
+        EC.presence_of_element_located((By.ID, "Delete_reports")))
+        element.click()
+        time.sleep(1)
+        id_test = str(self.driver.find_element_by_id("delete_page_header").text)
+        self.assertEqual("Remove Reports",id_test)
+    """
+    def test_delete_reports_page_delete_buttom(self):
+        element = WebDriverWait(self.driver, 10).until(
+        EC.presence_of_element_located((By.ID, "Simple_Upload")))
+        element.click()
+
+        element2 = WebDriverWait(self.driver, 10).until(
+        EC.presence_of_element_located((By.ID, "choose_file")))
+        element2.send_keys("C://Users/User/Desktop/math (MS) Report 2018.docx")
+        time.sleep(3)
+
+        element3 = WebDriverWait(self.driver, 10).until(
+        EC.presence_of_element_located((By.ID, "sub")))
+        element3.click()
+        time.sleep(3)
+
+        element4 = WebDriverWait(self.driver, 10).until(
+        EC.presence_of_element_located((By.ID, "Delete_reports")))
+        element4.click()
+        time.sleep(3)
+        
+        element5 = self.driver.find_element_by_type("submit")
+        element5.click()
+        time.sleep(2)
+
+        id_test = str(self.driver.find_element_by_tag_name("tbody").text)
+        self.assertEqual("",id_test)
+
+
 
 
     def tearDown(self):
